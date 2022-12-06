060d85de3a76209ded8d9f83beac7da159ac2c4c
DungDev0106 <dungnvph18102@fpt.edu.vn>
2022-12-07 03:25:11 +0700

    model_product update

--- model/model_product.php
+++ model/model_product.php
@@ -1,6 +1,6 @@
 <?php
-    function add_pro($pro_name, $pro_price, $target_file, $pro_desc, $cate_id){
-        $sql_add_pro = "INSERT INTO `products` (`pro_id`, `pro_name`, `pro_price`, `pro_image`, `pro_desc`, `cate_id`) VALUES (NULL, '{$pro_name}', '{$pro_price}', '{$target_file}', '{$pro_desc}','{$cate_id}')";
+    function add_pro($pro_name, $pro_price, $target_file, $pro_desc, $chat_lieu, $cate_id){
+        $sql_add_pro = "INSERT INTO `products` (`pro_id`, `pro_name`, `pro_price`, `pro_image`, `pro_desc`, `chat_lieu`, `cate_id`) VALUES (NULL, '{$pro_name}', '{$pro_price}', '{$target_file}', '{$pro_desc}', '{$chat_lieu}','{$cate_id}')";
         connect($sql_add_pro);
     }
 
@@ -22,11 +22,12 @@
         $one_pro = getOne($query_one_pro);
         return $one_pro;
     }
-    function update_pro($pro_id, $pro_name, $pro_price, $target_file, $pro_desc, $cate_id){
+// Cập nhật sản phẩm 
+    function update_pro($pro_id, $pro_name, $pro_price, $target_file, $pro_desc, $chat_lieu, $cate_id){
         if(empty($target_file)){
-            $update_pro = "UPDATE `products` SET pro_name='{$pro_name}', pro_price='{$pro_price}', pro_image = '{$target_file}, pro_desc = '{$pro_desc}', cate_id = '{$cate_id}' WHERE pro_id = '{$pro_id}'";
+            $update_pro = "UPDATE `products` SET pro_name='{$pro_name}', pro_price='{$pro_price}', pro_image = '{$target_file}, pro_desc = '{$pro_desc}', chat_lieu = '{$chat_lieu}' cate_id = '{$cate_id}' WHERE pro_id = '{$pro_id}'";
         } else{
-            $update_pro = "UPDATE `products` SET pro_name='{$pro_name}', pro_price='{$pro_price}', pro_desc = '{$pro_desc}', cate_id = '{$cate_id}' WHERE pro_id = '{$pro_id}'";
+            $update_pro = "UPDATE `products` SET pro_name='{$pro_name}', pro_price='{$pro_price}', pro_desc = '{$pro_desc}', chat_lieu = '{$chat_lieu}', cate_id = '{$cate_id}' WHERE pro_id = '{$pro_id}'";
         }
         connect($update_pro);
     }
@@ -34,10 +35,22 @@
         $delete_pro = "DELETE FROM `products` WHERE pro_id = '{$pro_id}'";
         connect($delete_pro);
     }
-
+// Lấy 9 sản phẩm hiển thị cho trang chủ
     function query_pro_home(){
         $select_pro = "SELECT * FROM `products` WHERE 1 ORDER BY pro_id LIMIT 0,9";
         $list_pro_home = getAll($select_pro);
         return $list_pro_home;
+    }
+// Top 10 sản phẩm ưu thích
+    function query_pro_top10(){
+        $sql = "SELECT * FROM `products` WHERE 1 ORDER BY pro_view DESC LIMIT 0,10";
+        $list_pro_top_10 = getAll($sql);
+        return $list_pro_top_10;
+    }
+// Lấy sản phẩm cùng loại(khác id nhưng cùng danh mục) theo lượt xem giảm dần
+    function query_similar_pro($pro_id, $cate_id){
+        $sql = "SELECT * FROM `products` WHERE pro_id <> '{$pro_id}' AND cate_id = '{$cate_id}' ORDER BY pro_view DESC";
+        $similar_pro = getAll($sql);
+        return $similar_pro;
     }
 ?>
