efb8f447c94820679df5df3daa420301b595e32a
DungDev0106 <dungnvph18102@fpt.edu.vn>
2022-12-07 14:46:29 +0700

    model_category update

--- model/model_category.php
+++ model/model_category.php
@@ -1,26 +1,30 @@
 <?php
-
-    function add_cate($cate_name){ // Hàm thêm danh mục
+// Thêm danh mục
+    function add_cate($cate_name){ 
         $sql_add_cate = "INSERT INTO `categories` VALUES (NULL, '$cate_name')";
         connect($sql_add_cate);
     }
-
-    function queryAll(){ // Hàm lấy tất cả danh mục
-        $query_cate = "SELECT * FROM categories ORDER BY cate_id";
+ // Hàm lấy tất cả danh mục
+    function queryAll(){
+        $query_cate = "SELECT * FROM `categories` ORDER BY cate_id";
         $list_cate = getAll($query_cate);
         return $list_cate;
     }
-    function queryOne($cate_id){ // Hàm lấy 1 danh mục
-        $query_one_cate = "SELECT * FROM categories  WHERE cate_id = '{$cate_id}'";
+// Hàm lấy 1 danh mục
+    function queryOne($cate_id){ 
+        $query_one_cate = "SELECT * FROM `categories`  WHERE cate_id = '{$cate_id}'";
         $one_cate = getOne($query_one_cate);
         return $one_cate;
     }
-    function update_cate($cate_id, $cate_name){ // Hàm cập nhật danh mục
-        $update_cate = "UPDATE categories SET cate_name = '{$cate_name}' WHERE cate_id = '{$cate_id}'";
+
+// Hàm cập nhật danh mục    
+    function update_cate($cate_id, $cate_name){ 
+        $update_cate = "UPDATE `categories` SET cate_name = '{$cate_name}' WHERE cate_id = '{$cate_id}'";
         connect($update_cate);
     }
-    function delete_cate($cate_id){ // Hàm xóa danh mục theo id
-        $delete_cate = "DELETE FROM categories WHERE cate_id = '{$cate_id}'";
+// Hàm xóa danh mục theo id    
+    function delete_cate($cate_id){ 
+        $delete_cate = "DELETE FROM `categories` WHERE cate_id = '{$cate_id}'";
         connect($delete_cate); 
     }
 
