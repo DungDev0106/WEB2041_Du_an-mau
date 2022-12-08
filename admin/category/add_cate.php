1a33c0d3cd717ddafb74abcc3b6088ab10840017
DungDev0106 <dungnvph18102@fpt.edu.vn>
2022-12-09 01:28:06 +0700

    add cate update

--- admin/category/add_cate.php
+++ admin/category/add_cate.php
@@ -4,28 +4,27 @@
             </p>
             <div class="form mt-5 leading-9">
                 <form action="index_admin.php?act=add_cate" method="POST" autocomplete="on">
-                    <p>Mã loại hàng</p>
-                    <input class="border w-full rounded-[4px] h-[40px] px-3 border-[#FFC0CB]"
+                    <label>Mã loại hàng</label>
+                    <input class="border w-full my-1 rounded-[4px] px-3 py-1 border-[#FFC0CB] "
                            type="text" disabled name="ma_loai"
                            placeholder="Auto number">
                     <label for="ten_loai">Tên loại hàng</label>
-                    <input class="border w-full rounded-[4px] h-[40px] px-3 border-[#FFC0CB]"
-                           type="text" name="ten_loai" required id = "ten_loai"
+                    <input class="border w-full mt-1 rounded-[4px] px-3 py-1 border-[#FFC0CB]"
+                           type="text" name="ten_loai" required id="ten_loai"
                            title="Tên loại hàng"
                            placeholder="Tên loại hàng..">
                     
-                    <input class=" border px-3 py-1  mt-3 rounded-[4px] bg-[#FFC0CB] hover:font-[500]"
-                           type="submit" value="Lưu lại" name="add_cate">
-                    <input class=" border px-3 py-1 rounded-[4px] bg-[#FFC0CB] hover:font-[500]"
-                           type="reset" value="Nhập lại" name="nhap_lai">
-                    <a href="index_admin.php?act=list_cate"
-                           class=" border px-3 py-[10.5px] rounded-[4px] bg-[#FFC0CB] hover:font-[500]">
-                        Danh sách danh mục
-                    </a>
-                    <a href="index_admin.php?act=add_pro"
-                           class=" border ml-[5px] px-3 py-[10.5px] rounded-[4px] bg-[#FFC0CB] hover:font-[500]">
-                        Thêm mới sản phẩm
-                    </a>
+                    <div class="button mt-3 text-[19px]">
+                        <input class=" border px-3 py-1  mt-3 rounded-[4px] bg-[#FFC0CB] hover:font-[500]"
+                            type="submit" value="Lưu lại" name="add_cate">
+                        <input class=" border px-3 py-1 rounded-[4px] bg-[#FFC0CB] hover:font-[500]"
+                            type="reset" value="Nhập lại" name="nhap_lai">
+                        <input class=" border px-3 py-1 rounded-[4px] bg-[#FFC0CB] hover:font-[500]"
+                               type="button" onclick="location.href='index_admin.php?act=list_cate'" value="Danh sách danh mục">
+                        <input class=" border px-3 py-1 rounded-[4px] bg-[#FFC0CB] hover:font-[500]"
+                               type="button" onclick="location.href='index_admin.php?act=add_pro'" value="Thêm mới sản phẩm">
+                        
+                    </div> <!-- End .button-->
                 </form>
                 <?php
                     if(isset($thong_bao) && ($thong_bao!= "")) echo $thong_bao;
